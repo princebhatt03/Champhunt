0999f3e71a951399e5f287c7da577488ba15b45e
Prince Bhatt <prince25io044@satiengg.in>
2024-01-28 23:16:49 +0530

    Added Page1 Content, GSAP and some images

--- script.js
+++ script.js
@@ -15,3 +15,55 @@ function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }
+var tl = gsap.timeline();
+
+tl.from('h1 ', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h2 ', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h3 ', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h4 ', {
+  y: -30,
+  duration: 0.8,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('p ', {
+  x: -30,
+  duration: 0.8,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+// tl.from('#btn2', {
+//   y: -30,
+//   duration: 0.8,
+//   delay: 0.3,
+//   stagger: 0.3,
+//   opacity: 0,
+// });
+
+// tl.from('h5', {
+//   x: -30,
+//   duration: 0.8,
+//   delay: 0.3,
+//   stagger: 0.3,
+//   opacity: 0,
+// });

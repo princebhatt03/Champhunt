ff0f5cac086163a256dcf4ccbb7ec15d9346818d
Prince Bhatt <prince25io044@satiengg.in>
2024-01-28 13:21:13 +0530

    Responsive Navbar Done

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamMenu');
+const navMenu = document.querySelector('#navbar ul');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+const navLink = document.querySelectorAll('#navbar a');
+
+navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
